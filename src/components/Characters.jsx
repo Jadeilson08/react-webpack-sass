817a0f10d34b033a6bd48c1b5908eb803797269d
Jadeilson08 <jadeilson08@gmail.com>
2021-08-09 17:59:36 -0400

    fetch

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,31 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { ajax } from 'rxjs/ajax';
-import { map, catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
-
-const obs$ = ajax.getJSON(`https://api.github.com/users?per_page=5`).pipe(
-  map(userResponse => userResponse),
-  catchError(error => {
-    console.log('error: ', error);
-    return of(error);
-  })
-);
 
 const Characters = () => {
-  const [character, setCharacter] = useState();
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
-    const sub = obs$.subscribe(setCharacter);
-
-    console.log(character);
-    return sub.unsubscribe();
+    fetch(`https://rickandmortyapi.com/api/character`)
+      .then(response => response.json())
+      .then(data => setCharacter(data.results));
   }, []);
 
   return (
-    <>
-      <h1>characeter</h1>
-      <h2>foo</h2>
-    </>
+    <div className="container">
+      <div className="characters">
+        {character.map(item => {
+          <div className="box" key={item.id}>
+            <img src="{item.image}" alt="" />
+          </div>;
+        })}
+      </div>
+    </div>
   );
 };
 
