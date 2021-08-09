c7b3c97d8280e0eae1497de6348b649108a83b85
Jadeilson08 <jadeilson08@gmail.com>
2021-08-09 14:36:53 -0400

    Configuracao webpack

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
-import "./style.css";
-
+import React from 'react';
+import './App.scss';
 
 const App = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="app">
+      <h1>React + Webpack + SASS</h1>
+      <p>My first project with React</p>
+    </div>
+  );
+};
 export default App;
