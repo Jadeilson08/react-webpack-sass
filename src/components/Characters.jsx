adf664ce78a166fdad801222781075a4d7c8c09c
Jadeilson08 <jadeilson08@gmail.com>
2021-08-09 17:30:58 -0400

    testando rxjs

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,7 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { ajax } from 'rxjs/ajax';
+import { map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+
+const obs$ = ajax.getJSON(`https://api.github.com/users?per_page=5`).pipe(
+  map(userResponse => userResponse),
+  catchError(error => {
+    console.log('error: ', error);
+    return of(error);
+  })
+);
 
 const Characters = () => {
-  const [character, setCharacter] = useState([]);
+  const [character, setCharacter] = useState();
+
+  useEffect(() => {
+    const sub = obs$.subscribe(setCharacter);
+
+    console.log(character);
+    return sub.unsubscribe();
+  }, []);
 
   return (
     <>
